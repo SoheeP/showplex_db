e45ea57fef9587423e4cd8632fa9a9750af1acfe
soheeP <psh950502@naver.com>
2020-02-05 22:04:20 +0900

    add mysql config

---
+++ routes/database/mysql.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+
+// db config
+var db = mysql.createConnection({
+  host: '127.0.0.1',
+  user: "showplex",
+  password: "root",
+  port: 3306,
+  database: "showplex"
+});
+db.connect();
+
+function query(queryState,callback = ()=>{}){
+  db.query(queryState,(err,rows)=>{
+    if(err) throw err;
+    let result = JSON.parse(JSON.stringify(rows));
+    callback(result)
+  })
+}
+
+
+
+exports.db = db;
+exports.query = query;
