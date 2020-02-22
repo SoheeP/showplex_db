714ea698175205a4e874d3de8b8a625ff37730e6
SoheeP <psh950502@naver.com>
2020-02-22 23:55:15 +0900

    implement board contents enroll

--- routes/board.js
+++ routes/board.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
+const moment = require('moment');
 const { db, query } = require('./database/mysql');
 
 router.post('/write', (req, res, next) => {
-  res.json({result : 1});
-})
+  let username = req.body.username,
+  title = req.body.title,
+  contents = req.body.contents,
+  time = moment().format('YYYY/MM/DD hh:mm:ss');
+  db.query(`select * from showplex.user where username="${username}"`, (error, results) => {
+    if(error) throw error;
+    if(results.length > 0){
+      query(`INSERT INTO showplex.freeboard (title, contents, author, time) VALUES ("${title}", "${contents}", "${username}", "${time}")`)
+      console.log(req.body);
+      res.json({ result : 1 });
+    } else {
+      res.json({ result: 2 });
+    };
+  });
+});
 
 module.exports = router;
