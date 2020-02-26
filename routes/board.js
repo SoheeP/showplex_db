5e397ff3cc4732b55e93ba938c49a130b602e62e
SoheeP <psh950502@naver.com>
2020-02-27 00:04:08 +0900

    implement delete post

--- routes/board.js
+++ routes/board.js
@@ -12,17 +12,32 @@ router.get('/freeboard', (req, res, next) => {
 })
 
 router.post('/write', (req, res, next) => {
-  let username = req.body.username,
-  title = req.body.title,
-  contents = req.body.contents,
-  time = moment().format('YYYY/MM/DD hh:mm:ss');
-  db.query(`select * from showplex.user where username="${username}"`, (error, results) => {
+  let { usernum, username, title, contents } = req.body;
+  let time = moment().format('YYYY-MM-DD hh:mm:ss');
+  db.query(`select * from showplex.user where usernum="${usernum}"`, (error, results) => {
     if(error) throw error;
     if(results.length > 0){
-      query(`INSERT INTO showplex.freeboard (title, contents, author, time) VALUES ("${title}", "${contents}", "${username}", "${time}")`)
+      query(`INSERT INTO showplex.freeboard (usernum, title, contents, author, time) VALUES ("${usernum}", "${title}", "${contents}", "${username}", "${time}")`)
       console.log(req.body);
       res.json({ result : 1 });
     } else {
+      res.json({ result: 2 });
+    };
+  });
+});
+
+router.post('/delete', (req, res, next) => {
+  let { usernum, id } = req.body;
+  db.query(`select * from showplex.user where usernum="${usernum}"`, (error, results) => {
+    if(error) throw error;
+    if(results.length > 0){
+      query(`DELETE FROM showplex.freeboard WHERE id='${id}' and usernum = ${usernum};`, (results) => {
+        //삭제 완료
+        console.log(req.body);
+        res.json({ result : 1 });
+      })
+    } else {
+      //삭제 실패
       res.json({ result: 2 });
     };
   });
